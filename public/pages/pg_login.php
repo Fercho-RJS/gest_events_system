6c924c62eac89a1ee20bb3c352867aaf994f033c
David Rojas <fernandorojas.contacto@gmail.com>
2025-06-11 12:12:42 -0300

    root custom

--- public/pages/pg_login.php
+++ public/pages/pg_login.php
@@ -1,25 +1,45 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Iniciar sesión  |  E-Web.net</title>
 
-  <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
-  <link rel="stylesheet" href="node_modules/bootstrap-icons/font/bootstrap-icons.min.css">
-  <link rel="icon" href="public/img/favicon/icon.png" type="image/x-icon">
+  <link rel="stylesheet" href="../../node_modules/bootstrap/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="../../node_modules/bootstrap-icons/font/bootstrap-icons.min.css">
+  <link rel="icon" href="../../public/img/favicon/icon.png" type="image/x-icon">
 
   <!--  Mantener al final  -->
-  <link rel="stylesheet" href="css/custom-index.css">
-  <link rel="stylesheet" href="css/custom-navbar.css">
+    <link rel="stylesheet" href="http://localhost/gest_events_system/css/custom-navbar.css">
+  <link rel="stylesheet" href="http://localhost/gest_events_system/css/custom-login.css">
 </head>
 
 <body>
-  <header>
+  <header id="general-login-header">
     <?php
-      require_once 'php/components/navbar.php';
+      require_once '../../php/components/navbar.php';
     ?>
   </header>
+
+  <section id="general-login-body">
+    <div id="login-container">
+      <div id="login-header" class="bg-primary">
+        <h4 class="text-white">Acceder a tu cuenta</h4>
+      </div>
+      <div id="login-body">
+        <form action="#" method="POST">
+
+        </form>
+      </div>
+      <div id="login-foot">
+
+      </div>
+    </div>
+  </section>
+
+  <footer id="general-login-foot">
+
+  </footer>
   
 </body>
 
